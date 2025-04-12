1f22a881e077ec1d2531ff659c907717c1c091f0
Afraz <afraz.btech@gmail.com>
2025-04-12 15:29:51 +0530

    files Updated

--- data/BQ/bronze.sql
+++ data/BQ/bronze.sql
@@ -4,31 +4,31 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.departments_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/departments/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-a/departments/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.encounters_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/encounters/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-a/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.patients_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/patients/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-a/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.providers_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/providers/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-a/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.transactions_ha` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-a/transactions/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-a/transactions/*.json']
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
@@ -36,31 +36,31 @@ OPTIONS (
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.departments_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/departments/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-b/departments/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.encounters_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/encounters/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-b/encounters/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.patients_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/patients/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-b/patients/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.providers_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/providers/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-b/providers/*.json']
 );
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `avd-practice-1.Project_Bronze.transactions_hb` 
 OPTIONS (
   format = 'JSON',
-  uris = ['gs://healthcare-bucket-22032025/landing/hospital-b/transactions/*.json']
+  uris = ['gs://healthcare-bucket-12042025/landing/hospital-b/transactions/*.json']
 );
 
 ---------------------------------------------------------------------------------------------------------------------------
